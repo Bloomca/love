4e76a45c3410b44abea962024bb79711152615f9
Seva Zaikov <seva.zaikov@gmail.com>
2025-08-02 14:24:07 -0700

    handle backtab key (#55)

--- src/app_state/ui.rs
+++ src/app_state/ui.rs
@@ -8,6 +8,8 @@ use crossterm::{
     execute,
 };
 
+use super::app::Config;
+
 pub struct FileEntry {
     pub path: PathBuf,
 }
@@ -321,6 +323,77 @@ impl UIState {
                 true
             }
             None => false,
+        }
+    }
+
+    pub fn handle_backtab_key(&mut self, config: &Config) {
+        if config.tabs_to_spaces {
+            if let Some(selection) = &mut self.selection {
+                let (start_line, start_column) = selection.start;
+                let (end_line, end_column) = selection.end;
+                let mut start_line_removed = 0;
+                let mut end_line_removed = 0;
+
+                for line_num in start_line.min(end_line)..=start_line.max(end_line) {
+                    if let Some(line) = self.lines.get_mut(line_num - 1) {
+                        let whitespaces = Self::calculate_whitespace_num(line);
+                        let remove_num = whitespaces.min(config.whitespaces_amount);
+
+                        if remove_num != 0 {
+                            line.drain(0..remove_num);
+
+                            if line_num == start_line {
+                                start_line_removed = remove_num;
+                            }
+
+                            if line_num == self.cursor_line {
+                                if self.cursor_column <= remove_num {
+                                    self.cursor_column = 1;
+                                } else {
+                                    self.cursor_column -= remove_num;
+                                }
+                            }
+
+                            if line_num == end_line {
+                                end_line_removed = remove_num;
+                            }
+                        }
+                    }
+                }
+
+                if start_line_removed != 0 {
+                    if start_column <= start_line_removed {
+                        selection.start = (start_line, 1);
+                    } else {
+                        selection.start = (start_line, start_column - start_line_removed);
+                    }
+                }
+
+                if end_line_removed != 0 {
+                    if end_column <= end_line_removed {
+                        selection.set_end(end_line, 1);
+                    } else {
+                        selection.set_end(end_line, end_column - end_line_removed);
+                    }
+                } else if start_line_removed != 0 {
+                    selection.set_end(end_line, end_column);
+                }
+            } else if let Some(line) = self.lines.get_mut(self.cursor_line - 1) {
+                let whitespaces = Self::calculate_whitespace_num(line);
+                let remove_num = whitespaces.min(config.whitespaces_amount);
+
+                if remove_num != 0 {
+                    line.drain(0..remove_num);
+
+                    if self.cursor_column <= remove_num {
+                        self.cursor_column = 1;
+                    } else {
+                        self.cursor_column -= remove_num;
+                    }
+                }
+            }
+        } else {
+            // pass for now
         }
     }
 }
