2b1ca0c8e47ddd685ad47972d4b5373118aba4bf
Seva Zaikov <seva.zaikov@gmail.com>
2025-08-03 12:58:44 -0700

    add cursor position info to status bar

--- src/app/render_app_layout.rs
+++ src/app/render_app_layout.rs
@@ -1,12 +1,12 @@
 use ratatui::{
     Frame,
     layout::{Constraint, Layout},
-    widgets::Block,
 };
 
 use crate::app_state::AppState;
 use crate::editor::render_editor;
 use crate::file_tree::render_file_tree;
+use crate::status_bar::render_status_bar;
 
 /// Render general application layout without any specific details.
 /// For now the layout is fixed, but the plan is to allow full customization
@@ -15,7 +15,7 @@ pub(super) fn render(frame: &mut Frame, app_state: &mut AppState) {
     let vertical = Layout::vertical([Constraint::Fill(1), Constraint::Length(3)]);
     let [main_area, status_area] = vertical.areas(frame.area());
 
-    frame.render_widget(Block::bordered(), status_area);
+    render_status_bar(frame, status_area, app_state);
 
     let horizontal = Layout::horizontal([Constraint::Length(50), Constraint::Fill(1)]);
     let [left_area, right_area] = horizontal.areas(main_area);
